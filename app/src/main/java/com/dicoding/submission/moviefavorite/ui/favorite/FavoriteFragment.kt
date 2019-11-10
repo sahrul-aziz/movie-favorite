b9e2e6bedafa01ff8975bf1ebe025fc115ca83f8
sahrul-aziz <sahrul-aziz@users.noreply.github.com>
2019-11-10 19:07:28 +0700

    Initial project preparation

---
+++ app/src/main/java/com/dicoding/submission/moviefavorite/ui/favorite/FavoriteFragment.kt
@@ -0,0 +1,31 @@
+package com.dicoding.submission.moviefavorite.ui.favorite
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.dicoding.submission.moviefavorite.R
+
+class FavoriteFragment : Fragment() {
+
+    private lateinit var favoriteViewModel: FavoriteViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        favoriteViewModel =
+            ViewModelProviders.of(this).get(FavoriteViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_favorite, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        favoriteViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/com/dicoding/submission/moviefavorite/ui/movie/MovieFragment.kt
@@ -0,0 +1,31 @@
+package com.dicoding.submission.moviefavorite.ui.movie
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.dicoding.submission.moviefavorite.R
+
+class MovieFragment : Fragment() {
+
+    private lateinit var movieViewModel: MovieViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        movieViewModel =
+            ViewModelProviders.of(this).get(MovieViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_movie, container, false)
+        val textView: TextView = root.findViewById(R.id.text_home)
+        movieViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
